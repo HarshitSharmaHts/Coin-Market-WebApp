d4999e8f5f83350b5523d44db6513f2435249fbe
Harshit Sharma <harshitsharmahts@gmail.com>
2018-04-28 04:03:32 +0530

    optimizing imports

--- server/src/main/java/com/coinmarket/server/RESTService.java
+++ server/src/main/java/com/coinmarket/server/RESTService.java
@@ -1,13 +1,11 @@
 package com.coinmarket.server;
 
-import java.util.ArrayList;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
