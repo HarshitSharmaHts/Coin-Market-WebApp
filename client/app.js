b839f8f09820201b95fc474c6f65215f7fe4fd96
Harshit Sharma <harshitsharmahts@gmail.com>
2018-04-28 14:41:22 +0530

    signup page integration

--- client/app.js
+++ client/app.js
@@ -1,12 +1,15 @@
+// main app module
 var app =angular.module('main',['ngRoute']);
 
+// module configuration
 app.config(function ($routeProvider,$locationProvider){
+
   $routeProvider.when('/',{
 
     templateUrl:'./view/login.html',
     controller:'loginController'
 
-  }).when('/register',{
+  }).when('/signup',{
 
     templateUrl:'./view/signup.html',
     controller:'signupController'
@@ -18,9 +21,37 @@ app.config(function ($routeProvider,$locationProvider){
   });
 
   $locationProvider.html5Mode(true);
+
 });
 
+// signupController controller for signup.html
+app.controller('signupController',function($scope, $location, $http){
+  $scope.signup = function() {
+    var name = $scope.name;
+    var email = $scope.email;
+    var password = $scope.password;
 
+    console.log(name);
+    console.log(email);
+    console.log(password);
+
+    $http({
+      url: 'http://localhost:8080/server/signup',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      data: 'name='+name+'&email='+email+'&password='+password
+    }).then(function(response) {
+      console.log(response.data);
+    });
+  }
+  $scope.goToLogin = function() {
+    $location.path('/');
+  }
+});
+
+// loginController controller for login.html
 app.controller('loginController',function ($scope, $location, $http){
 
   $scope.login = function() {
@@ -44,6 +75,6 @@ app.controller('loginController',function ($scope, $location, $http){
   }
 
   $scope.goToSignup = function () {
-    $location.path('/register');
+    $location.path('/signup');
   }
 });
